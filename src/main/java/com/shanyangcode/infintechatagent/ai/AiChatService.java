dd1f7a3c9d1b28294730876494b4295c1c286e00
orange <799243133@qq.com>
2026-03-03 22:37:56 +0800

    引入联网搜索

--- src/main/java/com/shanyangcode/infintechatagent/ai/AiChatService.java
+++ src/main/java/com/shanyangcode/infintechatagent/ai/AiChatService.java
@@ -1,8 +1,15 @@
 package com.shanyangcode.infintechatagent.ai;
 
+import java.util.List;
+
+
+
+import com.shanyangcode.infintechatagent.ai.AiChat;
+import com.shanyangcode.infintechatagent.tool.TimeTool;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.mcp.McpToolProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
@@ -13,8 +20,6 @@ import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class AiChatService {
 
@@ -24,17 +29,20 @@ public class AiChatService {
     @Resource
     private EmbeddingStore<TextSegment> embeddingStore;
 
+    @Resource
+    private McpToolProvider mcpToolProvider;
+
     @Bean
     public AiChat aiChat() {
-
         List<Document> documents = FileSystemDocumentLoader.loadDocuments("src/main/resources/docs");
         EmbeddingStoreIngestor.ingest(documents, embeddingStore);
 
         return AiServices.builder(AiChat.class)
                 .chatModel(chatModel)
-                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
                 .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
+                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                .tools(new TimeTool())
+                .toolProvider(mcpToolProvider)
                 .build();
     }
-
 }
